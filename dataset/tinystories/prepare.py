e2030d22d0744407fd6a7e313b0e10790e5a6d02
OsamaAlkhodairy <osama.khodairy@hotmail.com>
2023-09-01 14:14:30 +0200

    preparing dataset

--- dataset/tinystories/prepare.py
+++ dataset/tinystories/prepare.py
@@ -2,4 +2,15 @@ from datasets import load_dataset
 
 dataset = load_dataset('roneneldan/TinyStories')
 
-print(dataset)
+def filter(text):
+    return text.replace('\n\n', '\n')
+
+print(len(dataset['train']['text']))
+
+stories = dataset['train']['text']
+
+num_stories = len(stories)
+
+with open('train.txt', 'w') as file:
+    for i in range(num_stories):
+        print(''.join([filter(stories[i]), '\n']), file=file)
